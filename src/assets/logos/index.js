0d5bdd0188d844721a28cf31bc494d214a934de0
smit2308 <62828738+smit2308@users.noreply.github.com>
2023-12-24 18:40:45 -0800

    Create index.js

---
+++ src/assets/logos/index.js
@@ -0,0 +1,30 @@
+// Import statements
+import cSharpLogo from './icons8-c-sharp-logo.svg';
+import cLogo from './icons8-c.svg';
+import cssLogo from './icons8-css.svg';
+import firebaseLogo from './icons8-firebase.svg';
+import flutterLogo from './icons8-flutter.svg';
+import htmlLogo from './icons8-html.svg';
+import javaLogo from './icons8-java.svg';
+import javascriptLogo from './icons8-javascript.svg';
+import pythonLogo from './icons8-python.svg';
+import reactLogo from './icons8-react.svg';
+import sqlLogo from './icons8-sql.png';
+import unityLogo from './icons8-unity.svg';
+
+
+// Export statements
+export {
+  cSharpLogo,
+  cLogo,
+  cssLogo,
+  firebaseLogo,
+  flutterLogo,
+  htmlLogo,
+  javaLogo,
+  javascriptLogo,
+  pythonLogo,
+  reactLogo,
+  sqlLogo,
+  unityLogo,
+};
