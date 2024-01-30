fedca970c58096ad7009dd483dde50e134422c45
smit2308 <62828738+smit2308@users.noreply.github.com>
2024-01-29 20:54:43 -0800

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -71,7 +71,7 @@ Save time and find the information you need
                 {/* <div className="elfsight-app-ca5e72b0-9f8a-4038-b264-2d16ced03ce4 z-0  object-cover bg-black" data-elfsight-app-lazy></div>
                
                 <div className='absolute self-end  sel w-60 h-1  rounded-full bg-white z-20 justify-center items-center text-center text-primary'> </div> */}
-   <div className='absolute self-end  w-1/2 h-10 rounded-full bg-black z-60 justify-center items-center text-center text-primary'> </div> 
+   <div className='absolute self-end  w-1/2 h-10 rounded-full bg-primary z-60 justify-center items-center text-center text-primary'> </div> 
    <div id="curator-feed-default-feed-layout" >
       <a href="https://curator.io" target="_blank" rel="noopener noreferrer" className="crt-logo crt-tag text-white">
         Powered by Curator.io
